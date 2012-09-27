c3332f049f51ed10feaa5fc9c40ecbadea2d05d8
Brandon Lee <superscience890@gmail.com>
2012-09-27 17:17:22 -0400

    Added comments
    Commented calc.c and math.c

--- Lab 1/calc/calc.c
+++ Lab 1/calc/calc.c
@@ -1,3 +1,18 @@
+/*
+ * calc.c
+ *
+ * Brandon Lee - bfl
+ * Christopher Palmer - ctpalmer
+ * Joogyoon Han - jongyoo1
+ * Lawrence Tsang - ltsang
+ *
+ * Recreates the function of a calculator.  It accepts the input of a number,
+ * operation, then another number, with whitespace in between.  If it's one of
+ * the known operations, add, subtract, divide, multiply, or mod, then it will
+ * perform the said operation on a new line.  If input is not in the format 
+ * stated above, it will exit with the return value 0.\
+ *
+ */
 #include <stdio.h>
 #include "math.h"
 
@@ -6,31 +21,33 @@ int main() {
   char op;
 
   while(cont == 0) {
+    /* Ensures correct formatting with number operation number */
     if(fscanf(stdin, "%d %c %d", &first, &op, &second) != 3)
       cont = 1;
     else {
+	  /* Looks at the operation inputted and moves from there */
       switch(op) {
-        case '+':
+        case '+': // Refers to additive function
           result = add(first, second);
           printf("%d\n", result);
           break;
-        case '-':
+        case '-': // Refers to subtract function
           result = subtract(first, second);
           printf("%d\n", result);
           break;
-        case '*':
+        case '*': // Refers to multiply function
           result = multiply(first, second);
           printf("%d\n", result);
           break;
-        case '/':
+        case '/': // Refers to division function
           result = divide(first, second);
-          printf("%d\n", result);
+         printf("%d\n", result);
           break;
-        case '%':
+        case '%': // Referst to modulo function
           result = modulo(first, second);
           printf("%d\n", result);
           break;
-        default :
+        default : // If it's not anything as expected, exit out of the loop
           cont = 1;
       }
     }  
--- Lab 1/calc/math.c
+++ Lab 1/calc/math.c
@@ -1,3 +1,15 @@
+/*
+ * math.c
+ *
+ * Brandon Lee - bfl
+ * Christopher Palmer - ctpalmer
+ * Joogyoon Han - jongyoo1
+ * Lawrence Tsang - ltsang
+ *
+ * Helper function for calc.c.  Has a separate function for adding, subtracting
+ * multiplying, dividing, and modulating. 
+ *
+ */
 #include <stdio.h>
 #include "math.h"
 
