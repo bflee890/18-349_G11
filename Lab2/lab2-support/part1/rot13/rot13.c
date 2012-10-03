16a07584bea74463e7b9d063f5c18a48bb467c34
Lawrence Tsang <lawlrence@gmail.com>
2012-10-03 17:22:52 -0400

    Added Starter Code

---
+++ Lab2/lab2-support/part1/rot13/rot13.c
@@ -0,0 +1,13 @@
+/*
+ * rot13.c: ROT13 cipher test application
+ *
+ * Authors: Group Member 1 <email address>
+ *          Group Member 2 <email address>
+ * Date:    The current time & date
+ */
+
+int main(void) {
+	/* Needs more code. */
+
+	return -255;
+}
---
+++ Lab2/lab2-support/part2/kernel/kernel.c
@@ -0,0 +1,13 @@
+/*
+ * kernel.c: Kernel main (entry) function
+ *
+ * Author: Group Member 1 <email address>
+ *         Group Member 2 <email address>
+ * Date:   The current time & date
+ */
+
+int main(int argc, char *argv[]) {
+	/* Needs more code. */
+
+	return -255;
+}
