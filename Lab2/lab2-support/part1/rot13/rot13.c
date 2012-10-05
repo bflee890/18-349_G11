2a57eef3e01f6e7a05365d807af8f68e409ed576
jyhan90 <jyhan90@gmail.com>
2012-10-05 16:05:07 -0300

    initial commit

--- Lab2/lab2-support/part1/rot13/rot13.c
+++ Lab2/lab2-support/part1/rot13/rot13.c
@@ -6,8 +6,40 @@
  * Date:    The current time & date
  */
 
+#include <stdio.h>
+#include <unistd.h>
+
 int main(void) {
-	/* Needs more code. */
+
+        char input [100];
+        char output [100];
+        int bytes_read;
+        int i;
+
+        while(1)
+        {
+                bytes_read = read(STDIN_FILENO, input, 100);
+                if(bytes_read == 1)
+                        return 0;
+                else if(bytes_read < 0)
+                        return 1;
+                for(i = 0; i < bytes_read-1; i++)
+                {
+                        if(input[i] > 64 && input[i] < 78)
+                                output[i] = input[i] + 13;
+                        else if(input[i] > 77 && input[i] < 91)
+                                output[i] = input[i] - 13;
+                        else if(input[i] > 96 && input[i] < 110)
+                                output[i] = input[i] + 13;
+                        else if(input[i] > 109 && input[i] < 123)
+                                output[i] = input[i] - 13;
+                        else
+                                output[i] = input[i];
+                }
+                output[i] = '\n';
+                output[i+1] = '\0';
+                write(STDOUT_FILENO, output, bytes_read);
+        }
 
 	return -255;
 }
