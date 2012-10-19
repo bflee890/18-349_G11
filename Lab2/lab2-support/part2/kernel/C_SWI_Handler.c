e804bbea2cb8479f0ef5a2c8bfb75e9a0b16948a
Christopher Palmer <topherpalmtree@gmail.com>
2012-10-19 16:48:36 -0400

    use #defines in c_swi_handler switch statement

--- Lab2/lab2-support/part2/kernel/C_SWI_Handler.c
+++ Lab2/lab2-support/part2/kernel/C_SWI_Handler.c
@@ -1,9 +1,15 @@
 void C_SWI_handler (unsigned swi_num, unsigned *regs)
 {
     switch (swi_num) {
-        case 1:     0x900001 break;
-        case 3:     0x900003 break;
-        case 4:     0x900004 break;
+        case EXIT_SWI:     
+		s_exit(regs);
+		 break;
+        case READ_SWI:     
+		s_read(regs);
+		break;
+        case WRITE_SWI:     
+		s_write(regs)
+		break;
         default:    puts("Error, no matching SWI number."); 
                     exit(0x0badc0de);
                     break;
