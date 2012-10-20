e7bd04c56cd44e6ebdc703d208c4ed8f725b74b0
Christopher Palmer <topherpalmtree@gmail.com>
2012-10-20 13:30:23 -0400

    Changed buffer boundary checking, used #defines for boundary checking. Included these bounds in swi.h, also efined the swi functions in swi.h, so c programs that want to use the kernel can access these functions by including swi.h .

--- Lab2/lab2-support/part2/kernel/read.c
+++ Lab2/lab2-support/part2/kernel/read.c
@@ -1,10 +1,9 @@
 #include <bits/types.h>
 #include "include/bits/errno.h"
 #include "include/bits/fileno.h"
-#include <exports.h>
+#include "swi.h"
 
-ssize_t read(int fd, void* buf, ssize_t count) {
-
+ssize_t read(int fd, const void* buf, ssize_t count) {
     int i;
     char hold;
     char *Buf = (char *)buf;
@@ -12,22 +11,20 @@ ssize_t read(int fd, void* buf, ssize_t count) {
     if (fd != STDIN_FILENO) 
         return -EBADF;
 
-    unsigned max_size = (unsigned)buf + (unsigned)count; 
-    if ( ((unsigned)buf < 0xa0000000) || (max_size > 0xa3ffffff) )
-        return -EFAULT;
+    unsigned max_addr = (unsigned)buf + (unsigned)count; 
+    if ( (max_addr > SDRAM_END) || (max_addr < SDRAM_BEGIN) ||
+         ((unsigned) buf > SDRAM_END) || ((unsigned)buf < SDRAM_BEGIN) )
+         return -EFAULT;
 
     for (i=0; i < count; i++) {
         hold = getc();
-
-        if (hold == 4) 
+        if (hold == 4) {
             return i;
-
-        if ((hold == 127) || (hold== '\b')) {
+        } else if ((hold == 127) || (hold== '\b')) {
             puts("\b \b");
 	    i-=2; 
             Buf[i+1] = 0;
-	} 
-	else if ((hold == '\r') || (hold == '\n')) {
+	} else if ((hold == '\r') || (hold == '\n')) {
                 Buf[i] = '\n';
                 putc('\n');
 		return i+1;
@@ -35,6 +32,5 @@ ssize_t read(int fd, void* buf, ssize_t count) {
         Buf[i] = hold;
         putc(hold);
     }
-
     return count;
 } 
--- Lab2/lab2-support/part2/kernel/write.c
+++ Lab2/lab2-support/part2/kernel/write.c
@@ -1,24 +1,22 @@
 #include <bits/types.h>
 #include "include/bits/errno.h"
 #include "include/bits/fileno.h"
-#include <exports.h>
+#include "swi.h"
 
-ssize_t write(int fd, const void *buf, size_t count)
+ssize_t write(int fd, const void *buf, ssize_t count)
 {
-	int i;
-	char *Buf = (char *)buf;
-	if (fd != STDOUT_FILENO)
-		return -EBADF;
-	unsigned max_size = (unsigned)buf + (unsigned)count;
-	if ( ((unsigned)buf > 0xa3ffffff) ||
-	     (((unsigned)buf > 0x00ffffff) && ((unsigned)buf < 0xa0000000)) ||
-	     (max_size > 0xa3ffffff) ||
-	     ((max_size > 0x00ffffff) && (max_size < 0xa0000000)) )
-        return -EFAULT;
+    int i;
+    char *Buf = (char *)buf;
+    if (fd != STDOUT_FILENO)
+        return -EBADF;
+    unsigned max_addr = (unsigned)buf + (unsigned)count;
+    if ( (max_addr > SDRAM_END) || (max_addr < SDRAM_BEGIN) ||
+         ((unsigned)buf > SDRAM_END) || ((unsigned)buf < SDRAM_BEGIN) )
+         return -EFAULT;
 
-	for(i=0; i<count; i++)
-	{
-		putc(Buf[i]); 
-	} 
-	return count;
+    for(i=0; i<count; i++)
+    {
+        putc(Buf[i]); 
+    } 
+    return count;
 }
