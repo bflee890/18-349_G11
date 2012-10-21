d0289d9947b2f5b0f214d72385159a7acbcbbfa3
Christopher Palmer <topherpalmtree@gmail.com>
2012-10-21 14:54:29 -0400

    exit should be declared as extern void.

--- Lab2/lab2-support/part2/kernel/swi_handler.h
+++ Lab2/lab2-support/part2/kernel/swi_handler.h
@@ -3,5 +3,5 @@
 #include <bits/types.h>
 ssize_t read(int fd,void* buf, size_t count);
 ssize_t write(int fd, const void *buf, size_t count);
-void exit(int status);
+extern void exit(int status);
 #endif
