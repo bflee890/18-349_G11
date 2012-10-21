58d8a79ae9f451a73aee67df5a355c013158ea42
jyhan90 <jyhan90@gmail.com>
2012-10-21 20:53:59 -0300

    changed echo function

--- Lab2/lab2-support/part2/echo/echo.c
+++ Lab2/lab2-support/part2/echo/echo.c
@@ -23,3 +23,29 @@ int main(int argc, char** argv) {
     exit(0);
     return 0;
 }
+
+/*#include "stdlib.h"
+#include "unistd.h"
+#include "bits/fileno.h"
+
+int main(int argc, char** argv) {
+
+        char buf[256];
+        int bytes_read;
+
+        while(1)
+        {
+                bytes_read = read(STDIN_FILENO, buf, 100);
+                if(bytes_read < 0) //return when error occurs.
+                        return 1;
+                if(bytes_read == 0 || bytes_read == 1) //exit when there is no input.
+                        return 0;
+
+                if(write(STDOUT_FILENO, buf, bytes_read) < 0)
+                        return 1; //return when error occurs.
+        }
+
+        /* Put your code here */
+
+        return -255;
+}*/
