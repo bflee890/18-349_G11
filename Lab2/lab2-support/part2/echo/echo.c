35409b956b256085ab196179ea3d326e4c8de534
Christopher Palmer <topherpalmtree@gmail.com>
2012-10-21 15:24:24 -0400

    Started echo.c, needs tested, updated Makefile to work with

--- Lab2/lab2-support/part2/echo/echo.c
+++ Lab2/lab2-support/part2/echo/echo.c
@@ -4,10 +4,22 @@
  * @date    The current date
  */
 
-
+#include "../kernel/include/bits/fileno.h"
+#include "../kernel/include/bits/errno.h"
+#include "../kernel/include/swi_handler.h"
 int main(int argc, char** argv) {
-
-	/* Put your code here */
-
-	return -255;
+    void* buf = (void*) (argv + argc*sizeof(char**));
+    size_t buf_size; 
+    ssize_t n = read(STDIN_FILENO,buf,buf_size )
+    if (n == -EFAULT || n == - ENFILE) {
+        exit(n);
+        return (n);   
+    }
+    n = write(STDOUT_FILENO,buf,n);
+    if (n == -EFAULT || n == - ENFILE) {
+        exit(n);
+        return (n);   
+    }
+    exit(0);
+    return 0;
 }
