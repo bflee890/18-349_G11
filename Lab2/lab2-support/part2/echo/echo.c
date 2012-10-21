77306e81551b91a9b64dd41e2e3925cb6fca0e67
Jongyoon Han <jyhan90@gmail.com>
2012-10-21 19:44:43 -0400

    x changed echo function.

--- Lab2/lab2-support/part2/echo/echo.c
+++ Lab2/lab2-support/part2/echo/echo.c
@@ -4,9 +4,27 @@
  * @date    The current date
  */
 
+#include "stdlib.h"
+#include "unistd.h"
+#include "bits/fileno.h"
 
 int main(int argc, char** argv) {
 
+	char buf[256];
+	int bytes_read;
+
+	while(1)
+	{
+		bytes_read = read(STDIN_FILENO, buf, 100);
+		if(bytes_read < 0) //return when error occurs.
+			return 1;
+		if(bytes_read == 0 || bytes_read == 1) //exit when there is no input.
+                        return 0;
+
+		if(write(STDOUT_FILENO, buf, bytes_read) < 0)
+			return 1; //return when error occurs.
+	}
+	
 	/* Put your code here */
 
 	return -255;
