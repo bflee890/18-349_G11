0168c82808430bbb273ebef82b3d1ad3174508f3
superscience890 <superscience890@gmail.com>
2012-10-21 20:09:27 -0400

    echo should work and swi_handler.h is removed

--- Lab2/lab2-support/part2/echo/echo.c
+++ Lab2/lab2-support/part2/echo/echo.c
@@ -4,27 +4,7 @@
  * @date    The current date
  */
 
-#include "../kernel/include/bits/fileno.h"
-#include "../kernel/include/bits/errno.h"
-#include "../kernel/swi_handler.h"
-int main(int argc, char** argv) {
-    void* buf = (void*) (argv + argc*sizeof(char**));
-    size_t buf_size = 100; 
-    ssize_t n = read(STDIN_FILENO,buf,buf_size );
-    if (n == -EFAULT || n == - ENFILE) {
-        exit(n);
-        return (n);   
-    }
-    n = write(STDOUT_FILENO,buf,n);
-    if (n == -EFAULT || n == - ENFILE) {
-        exit(n);
-        return (n);   
-    }
-    exit(0);
-    return 0;
-}
-
-/*#include "stdlib.h"
+#include "stdlib.h"
 #include "unistd.h"
 #include "bits/fileno.h"
 
@@ -37,15 +17,15 @@ int main(int argc, char** argv) {
         {
                 bytes_read = read(STDIN_FILENO, buf, 100);
                 if(bytes_read < 0) //return when error occurs.
-                        return 1;
+                        return bytes_read;
                 if(bytes_read == 0 || bytes_read == 1) //exit when there is no input.
                         return 0;
 
                 if(write(STDOUT_FILENO, buf, bytes_read) < 0)
-                        return 1; //return when error occurs.
+                        return bytes_read; //return when error occurs.
         }
 
         /* Put your code here */
 
         return -255;
-}*/
+}
