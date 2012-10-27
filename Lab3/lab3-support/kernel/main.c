17c2ff2676332a84af4a1dfd21cd23a02eda75f3
Brandon Lee <superscience890@gmail.com>
2012-10-27 17:31:32 -0400

    Added Lab 3 files
    All of the new support files for Lab 3

---
+++ Lab3/lab3-support/kernel/main.c
@@ -0,0 +1,18 @@
+#include <exports.h>
+
+#include <arm/psr.h>
+#include <arm/exception.h>
+#include <arm/interrupt.h>
+#include <arm/timer.h>
+
+uint32_t global_data;
+
+int kmain(int argc, char** argv, uint32_t table)
+{
+	app_startup(); /* bss is valid after this point */
+	global_data = table;
+
+	/* Add your code here */
+
+	return 0;
+}
---
+++ Lab3/lab3-support/tasks/splat/splat.c
@@ -0,0 +1,12 @@
+/** @file splat.c
+ *
+ * @brief Displays a spinning cursor.
+ *
+ * Links to libc.
+ */
+
+int main(int argc, char** argv)
+{
+	/* Add code here -- put your names at the top. */
+	return 0;
+}
