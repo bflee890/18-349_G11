b7b4f48104dda49ff270909e43e44fe5b060a0e1
Jongyoon Han <jyhan90@gmail.com>
2012-12-01 09:29:24 -0500

    Added lab3 files to lab4 with some modifications

---
+++ lab4/kernel/arm/c_irq_handler.c
@@ -0,0 +1,19 @@
+#include <types.h>
+#include <arm/interrupt.h>
+#include <arm/timer.h>
+#include <arm/reg.h>
+
+void c_irq_handler()
+{
+    //i_bit indicates if timer interrupt has occurred
+    uint32_t i_bit = reg_read(INT_ICPR_ADDR); 
+    uint32_t next_time;
+    if(i_bit & (1<<INT_OSTMR_0))
+    {
+	next_time = reg_read(OSTMR_OSMR_ADDR(0));
+	next_time += TICKS_FROM_MILLIS(10); // notes next interrupt time
+	reg_write(OSTMR_OSMR_ADDR(0), next_time); //writes it in to OSMR
+	reg_set(OSTMR_OSSR_ADDR, 0x1); // clear OSSR to allow interrupt
+	incrTimer(); // increases personal timer
+    }
+}
--- lab4/kernel/arm/timer.c
+++ lab4/kernel/arm/timer.c
@@ -1,5 +1,6 @@
 #include <arm/timer.h>
 #include <arm/reg.h>
+#include <types.h>
 
 volatile unsigned long timer;
 
@@ -18,4 +19,23 @@ void incrTimer(void) {
     timer += 1;
 }
 
+void destroy_timer(void) {
+    return;
+}
 
+void timer_handler(unsigned int int_numb) {
+
+    uint32_t next_time = reg_read(OSTMR_OSMR_ADDR(0));
+    next_time += get_ticks(); // notes next interrupt time
+    reg_write(OSTMR_OSMR_ADDR(0), next_time); //writes it in to OSMR
+    reg_set(OSTMR_OSSR_ADDR, 0x1); // clear OSSR to allow interrupt
+    incrTimer(); // increases personal timer
+}
+
+unsigned long get_ticks(void) {
+    return (OSTMR_FREQ/100*x);
+}
+
+unsigned long get_millis(void) {
+    return (x/OSTMR_FREQ*100);
+}
