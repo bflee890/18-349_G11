3118f9c38968d940213aa190c5a635f8fcbd1f00
Christopher Palmer <topherpalmtree@gmail.com>
2012-12-08 03:22:32 -0500

    added number of groups #define

--- lab4/kernel/include/config.h
+++ lab4/kernel/include/config.h
@@ -26,7 +26,7 @@
 #define IDLE_PRIO             63
 #define OS_MAX_TASKS          64
 #define OS_AVAIL_TASKS        63
-
+#define OS_NUM_GROUPS         8
 /* OS_NUM_MUTEX must be at lease 32 */
 #define OS_NUM_MUTEX	32
 
