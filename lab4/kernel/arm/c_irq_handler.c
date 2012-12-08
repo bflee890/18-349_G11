805f83d7972e5970722ec8e5b330c3eb4d0161e0
Christopher Palmer <topherpalmtree@gmail.com>
2012-12-07 21:13:07 -0500

    added local variable to keep track of next os_timer time and calls devupdate

--- lab4/kernel/arm/c_irq_handler.c
+++ lab4/kernel/arm/c_irq_handler.c
@@ -2,7 +2,7 @@
 #include <arm/interrupt.h>
 #include <arm/timer.h>
 #include <arm/reg.h>
-
+unsigned long os_next_timer=0;
 void c_irq_handler()
 {
     //i_bit indicates if timer interrupt has occurred
@@ -10,10 +10,10 @@ void c_irq_handler()
     uint32_t next_time;
     if(i_bit & (1<<INT_OSTMR_0))
     {
-	next_time = reg_read(OSTMR_OSMR_ADDR(0));
-	next_time += get_ticks(); // notes next interrupt time
-	reg_write(OSTMR_OSMR_ADDR(0), next_time); //writes it in to OSMR
+	os_next_time += get_ticks(); // notes next interrupt time
+	reg_write(OSTMR_OSMR_ADDR(0), os_next_time); //writes it in to OSMR
 	reg_set(OSTMR_OSSR_ADDR, 0x1); // clear OSSR to allow interrupt
 	incr_timer(); // increases personal timer
+        dev_update(get_clock);
     }
 }
